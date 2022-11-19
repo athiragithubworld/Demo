828699fc5d29cc91bcbb49265a954f69164d8e26
athiragithubworld <athirahrms@gmail.com>
2022-11-19 10:50:15 +0530

    first commit

---
+++ array.js
@@ -0,0 +1,12 @@
+// Create array with value
+let arr=['red','green','white'];
+// use push method to add value to array
+arr.push('blue');
+console.log( arr);
+// use pop method to delete last value to array
+arr.pop();
+console.log(arr);
+// use unshift method to add value to array in the begining
+arr.unshift('yellow');
+console.log(arr);
+// check the index value of greenconsole.log(arr.indexOf('green'));
